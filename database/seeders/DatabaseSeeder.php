78e7c226f2077d56c215a5a34c546e389aefdc16
shaonlimit <shasanexp@gmail.com>
2023-09-27 17:35:27 +0600

    seeders added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'Saiful',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin@gmail.com'),
+            'role_id' => 1
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Shaon',
+            'email' => 'controller@gmail.com',
+            'password' => Hash::make('controller@gmail.com'),
+            'role_id' => 2
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'Admin',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'Controller',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'Examinee',
+        ]);
     }
 }
